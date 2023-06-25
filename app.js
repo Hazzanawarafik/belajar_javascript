f9f8cdb2baa172ef2d8ceb0a5de62c79a8da47cd
hazzanawarafik <hazzanawa@gmail.com>
2023-06-25 11:00:50 +0700

    if statement pertama yang kmau pelajari

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-alert(1+1);
-console.log('hello world');
+// console.log("Sebelum pembanding")
+// if(1+1 === 2){
+//     console.log('Perhitungannya masih betul');
+// }
+// console.log('setelah pembanding');
+
+let angka = Math.random();
+console.log(angka);
+
+if(angka >= 0.5){
+    console.log("Angka lebih besar dari 0,5");
+}
+if(angka <= 0.5){
+    console.log("Angka lebih kecil dari 0,5");
+}
