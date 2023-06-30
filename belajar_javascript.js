26d914192e03ac541e976df3169130d4877598f0
hazzanawarafik <hazzanawa@gmail.com>
2023-06-30 21:42:38 +0700

    Mendapatkan Data Dari Nested Array Dengan Nested Loop

--- belajar_javascript.js
+++ belajar_javascript.js
@@ -438,17 +438,33 @@
 
 
 // ---------------------------------------------------- Nested Loop
-let str = 'LOL';
-for(let i=0; i<=4; i++){
-    console.log('Outer:',i);
-    for(let j=0; j<str.length; j++){
-        console.log('        Inner:',str[j]);
-    }
-}
-const pilihan = 'abcd';
-for(let i = 1; i<=10; i++){
-    console.log(`${i}.Soal Nomor ${i}: `);
-    for(let j = 0; j<pilihan.length; j++){
-        console.log(`    ${pilihan[j]}.Pilihan Jawaban`);
+// let str = 'LOL';
+// for(let i=0; i<=4; i++){
+//     console.log('Outer:',i);
+//     for(let j=0; j<str.length; j++){
+//         console.log('        Inner:',str[j]);
+//     }
+// }
+// const pilihan = 'abcd';
+// for(let i = 1; i<=10; i++){
+//     console.log(`${i}.Soal Nomor ${i}: `);
+//     for(let j = 0; j<pilihan.length; j++){
+//         console.log(`    ${pilihan[j]}.Pilihan Jawaban`);
+//     }
+// }
+
+
+// ---------------------------------------------------- Mendapatkan Data Dari Nested Array Dengan Nested Loop
+const studentRow = [
+    ['Olivia','Liam','Emma','Noah'],
+    ['Amelia','Oliver','Ava','Elijah'],
+    ['Sophia','Mateo','Isabella','Lucas'],
+];
+
+for(let i=0; i<studentRow.length; i++){
+    const row = studentRow[i];
+    console.log(`Seat Row #${i+1}`);
+    for(let j=0; j<row.length; j++){
+        console.log(`      ${row[j]}`);
     }
 }
